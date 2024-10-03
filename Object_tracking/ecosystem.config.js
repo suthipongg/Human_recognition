8ff5945d041cd5c6479bcb92733ce1dabd0dcce7
suthipongg <suthipong.mew@gmail.com>
2024-10-03 16:38:44 +0700

    update

--- Object_tracking/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [
-        {
-            name: 'service-python-object-tracking',
-            // Start the object tracking system
-            script: 'python3 app.py',
-        }
-    ]
-}
--- Object_tracking/script_nova.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512'
-source ~/nova/venv/bin/activate
-
-pm2 del service-python-object-tracking
-pm2 start
